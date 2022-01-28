6604c8f2b0d572a2f9c226fc8ea5db0f8f572684
seum <seumworldinbangladesh@gmail.com>
2022-01-29 02:07:08 +0600

    master auto first start done

---
+++ index.js
@@ -0,0 +1,11 @@
+const puppeteer = require('puppeteer')
+
+async function start() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://seumsims.github.io/test-req/")
+    await page.screenshot({ path: "screenshot.jpg" })
+    await browser.close() // if this code not written node will run untill somehow node stop
+}
+
+start()
